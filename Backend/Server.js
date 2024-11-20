76a9ba85c3be9d497fcdf2aaeef80afb03f47651
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-11-20 21:55:22 +0530

    CORS Issue Solved

--- Backend/Server.js
+++ Backend/Server.js
@@ -25,6 +25,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }))
+app.options('*', cors());
 app.use(bodyParser.json())
 app.use(helemt())
 app.use(compression())
