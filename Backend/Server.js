a67985f622ae45d2d3b2aba7bd1b0ed68e25102d
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-11-20 22:04:55 +0530

    updated

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,13 +19,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 //middlewares
+app.options('*', cors());
 app.use(cors({
     origin: ['https://idea-lab-chi.vercel.app/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }))
-app.options('*', cors());
 app.use(bodyParser.json())
 app.use(helemt())
 app.use(compression())
