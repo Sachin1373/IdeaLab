2f7d5cd655cd17fdbfb2a0a14d02d3ec757cb790
Sachin Rathod <sachinvrathod1373@gmail.com>
2025-12-05 15:45:38 +0530

    testing rate limit

--- Frontend/src/pages/Ideas.jsx
+++ Frontend/src/pages/Ideas.jsx
@@ -23,7 +23,16 @@ function Ideas() {
   };
 
   useEffect(() => {
+    // Call immediately on mount
     getAllIdeas();
+  
+    // Set interval to call every 1 minute
+    const interval = setInterval(() => {
+      getAllIdeas();
+    }, 6000); // 60000 ms = 1 minute
+  
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, []);
 
   const handleLoadMore = () => {
